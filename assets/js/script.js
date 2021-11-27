4deeffab0c6508da51ed9cf0e7fcc64d5aac05ca
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-11-26 20:00:30 -0700

    forms now store items for list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 console.log(buttonEl);
 
 
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
+
 
   var taskItemEl = document.createElement("li");
 
   var buttonEl = document.querySelector("#save-task");
   var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-  var createTaskHandler = function() {
+  var createTaskHandler = function(event) {
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
   
-  buttonEl.addEventListener("click", createTaskHandler);
-
+  formEl.addEventListener("submit", createTaskHandler);
  
